a229982b9e3c9f1aa1dc1e04a90f0f5461503645
Fábio Fantato <fabio.fantato@doitlean.com>
2023-05-30 11:11:19 +0100

    update -force parameter

--- DoiTLean.DiffJSON/generate_upload_package.ps1
+++ DoiTLean.DiffJSON/generate_upload_package.ps1
@@ -1,2 +1,2 @@
 dotnet publish -c Release -r linux-x64 --self-contained false
-Compress-Archive -Path .\bin\Release\net6.0\linux-x64\publish\* -DestinationPath ..\Dist\ODC-DiffJSON.zip
+Compress-Archive -Path .\bin\Release\net6.0\linux-x64\publish\* -DestinationPath ..\Dist\ODC-DiffJSON.zip -Force
